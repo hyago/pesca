07e3f1f4c4e9cfd85e2619b832b38abab238805d
stefano <stefanouesc@gmail.com>
2014-05-25 17:32:51 -0300

    MonitoramentoCompleto

--- application/controllers/ArrastoFundoController.php
+++ application/controllers/ArrastoFundoController.php
@@ -13,13 +13,33 @@ class ArrastoFundoController extends Zend_Controller_Action
         
         $this->usuarioLogado = Zend_Auth::getInstance()->getIdentity();
         $this->view->usuarioLogado = $this->usuarioLogado;
+        
+        
+        $this->modelPescador = new Application_Model_Pescador();
+        $this->modelBarcos = new Application_Model_Barcos();
+        $this->modelTipoEmbarcacao = new Application_Model_TipoEmbarcacao();
+        $this->modelPesqueiro = new Application_Model_Pesqueiro();
+        $this->modelEspecie = new Application_Model_Especie();
     }
 
     public function indexAction()
     {
-        // action body
+        $pescadores = $this->modelPescador->select(null, 'tp_nome');
+        $barcos = $this->modelBarcos->select();
+        $tipoEmbarcacoes = $this->modelTipoEmbarcacao->select();
+        $pesqueiros = $this->modelPesqueiro->select();
+        $especies = $this->modelEspecie->select();
+        
+        $this->view->assign('pescadores',$pescadores);
+        $this->view->assign('barcos',$barcos);
+        $this->view->assign('tipoEmbarcacoes',$tipoEmbarcacoes);
+        $this->view->assign('pesqueiros',$pesqueiros);
+        $this->view->assign('especies',$especies);
+    
     }
 
-
+    public function visualizarAction(){
+        
+    }
 }
 
---
+++ application/models/Barcos.php
@@ -0,0 +1,65 @@
+<?php
+
+class Application_Model_Barcos
+{
+private $dbTableBarcos;
+
+    public function select($where = null, $order = null, $limit = null)
+    {
+        $this->dbTableBarcos = new Application_Model_DbTable_Barcos();
+        $select = $this->dbTableBarcos->select()
+                ->from($this->dbTableBarcos)->order($order)->limit($limit);
+
+        if(!is_null($where)){
+            $select->where($where);
+        }
+
+        return $this->dbTableBarcos->fetchAll($select)->toArray();
+    }
+    
+    public function find($id)
+    {
+        $this->dbTableBarcos = new Application_Model_DbTable_Barcos();
+        $arr = $this->dbTableBarcos->find($id)->toArray();
+        return $arr[0];
+    }
+    
+    public function insert(array $request)
+    {
+        $this->dbTableBarcos = new Application_Model_DbTable_Barcos();
+        
+        $dadosBarcos = array(
+            'bar_nome' => $request['nomeBarco']
+        );
+        
+        $this->dbTableBarcos->insert($dadosBarcos);
+
+        return;
+    }
+    
+    public function update(array $request)
+    {
+        $this->dbTableBarcos = new Application_Model_DbTable_Barcos();
+        
+        $dadosBarcos = array(
+            'bar_nome' => $request['nomeBarco']
+        );
+        
+        $whereBarcos= $this->dbTableBarcos->getAdapter()
+                ->quoteInto('"bar_id" = ?', $request['idBarco']);
+        
+        $this->dbTableBarcos->update($dadosBarcos, $whereBarcos);
+    }
+    
+    public function delete($idBarcos)
+    {
+        $this->dbTableBarcos = new Application_Model_DbTable_Barcos();       
+                
+        $whereBarcos= $this->dbTableBarcos->getAdapter()
+                ->quoteInto('"bar_id" = ?', $idBarcos);
+        
+        $this->dbTableBarcos->delete($whereBarcos);
+    }
+
+}
+
