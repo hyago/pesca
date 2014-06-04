a31bc354597c4de267757f35fa27f37fd3a82b1d
mohonda <mohonda@localhost>
2014-06-04 10:05:50 -0300

    Criação da view ficha diária (usado em index).

--- bd/01_criacaoTabelasBD.sql
+++ bd/01_criacaoTabelasBD.sql
@@ -678,6 +678,27 @@ CREATE TABLE IF NOT EXISTS T_FICHA_DIARIA (
  CONSTRAINT FK_DSBQ_FICHA_DIARIA_DSBQ_VENTO1 FOREIGN KEY (VNT_ID)REFERENCES T_VENTO (VNT_ID)
 );
 
+CREATE VIEW V_FICHA_DIARIA AS
+SELECT FD.FD_ID, FD.FD_DATA, UPPER(SUBSTRING(FD.FD_TURNO, 1, 1)) AS FD_TURNO,
+FD.PTO_ID, TP.PTO_NOME,
+FD.TMP_ID, TEMPO.TMP_ESTADO,
+FD.VNT_ID, VENTO.VNT_FORCA,
+FD.T_ESTAGIARIO_TU_ID, TUE.TU_NOME AS T_ESTAGIARIO, 
+FD.T_MONITOR_TU_ID1, TUM.TU_NOME AS T_MONITOR
+FROM T_FICHA_DIARIA AS FD,
+T_PORTO AS TP,
+T_TEMPO AS TEMPO,
+T_VENTO AS VENTO,
+T_USUARIO AS TUE,
+T_USUARIO AS TUM
+WHERE FD.PTO_ID=TP.PTO_ID AND
+FD.TMP_ID=TEMPO.TMP_ID AND
+FD.VNT_ID=VENTO.VNT_ID AND
+FD.T_ESTAGIARIO_TU_ID=TUE.TU_ID AND
+FD.T_MONITOR_TU_ID1=TUM.TU_ID
+ORDER BY FD.FD_DATA DESC , TP.PTO_NOME ASC;
+
+
 -- -----------------------------------------------------
 -- TABLE T_MONITORAMENTO
 -- -----------------------------------------------------
@@ -2501,4 +2522,78 @@ CONSTRAINT T_T_ENTREVISTA_HAS_T_AVISTAMENTO_PKEY PRIMARY KEY (TE_ID,  FD_ID,  TA
 
 
 
-	
+CREATE TABLE t_proprietario_embarcacoes
+(
+  PROE_ID serial NOT NULL,
+  PROE_localdesembarque integer,
+  PROE_nfolha integer.
+    PROE_proprietario ,
+    PROE_mestre,
+    PROE_nomeembarcacao.
+    PROE_outrasembarcacoes *
+    PROE_MaxTritulantes
+    N máx de tripulantes
+    Tripulação
+    Nome ou Apelido Tripulantes mais frequentes
+    Há cozinheiro?
+    Estado de Conservação
+    Cor
+    Tipo
+    Comp. Total	
+    Comp. Boca	
+    Altura Calado	
+    Arqueadura Bruta	
+    N. Registro	
+    Licença de Captura	
+    Porto de Origem	
+    Material	
+    Tipo de casco	
+    Quando foi comprada	
+    Estado (nova ou usada)
+    Já está paga	
+    Como foi o pagamento	
+    Se financiado, por onde	
+    Ano Construção
+    Propulsão
+    Tipo Motor	
+    Modelo	
+    Marca	
+    Potência (Hp)
+    Tipo de combust.
+    Capac. Armazen. Combust.
+    Onde compra o combust.
+    Motor, quando foi comprado
+    Estado (novo ou usado)
+    Já está pago	
+    Como foi o pagamento
+    Se financiado, por onde
+    Ano de fabricação motor
+    Manutenção e Reparos
+    Obs
+    Gasto mensal com manutenção (R$)
+    Equipamentos	
+    Salvatagem	
+    Área de Atuação - Navegação	
+    Atuação batimétrica (prof)
+    Autonomia (dias máx no mar)
+    Freq. de Pescarias
+    Arte de Pesca
+    De quem são os petrechos
+    Horário da pesca (dia ou noite)
+    Capac. armazenar pescado (kg)
+    Conservação do pescado
+    Onde adquire
+    Como é feita a divisão do pescado
+    Destino do pescado
+    Vende para quem
+    Espécies maia capturadas
+    Espécies com maior valor comercial
+    Pega mais pescado no V ou I (Qtdade)
+    Pega mais pescado CARO no V ou I  (Qldade)
+    Como escolhe peixes que levar para casa e os que vende
+    Tem outra atividade de renda além da pesca
+    Há competição com outros pescadores
+    Há quanto tempo atua na atividade pesqueira (anos)
+    Data
+    Entrevistador
+);
