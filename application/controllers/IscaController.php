e4435ee4220fa5dd94be4f6ce648e559f003df49
mohonda <mohonda@notei7.nbcgib.uesc.br>
2014-06-27 12:01:54 -0300

    Menu de Relatórios e Relatório de Isca.

--- application/controllers/IscaController.php
+++ application/controllers/IscaController.php
@@ -4,36 +4,36 @@ class IscaController extends Zend_Controller_Action
 {
     private $usuario;
     public function init()
-    {   
+    {
         if(!Zend_Auth::getInstance()->hasIdentity()){
             $this->_redirect('index');
         }
-        
+
         $this->_helper->layout->setLayout('admin');
-        
-        
+
+
         $auth = Zend_Auth::getInstance();
          if ( $auth->hasIdentity() ){
           $identity = $auth->getIdentity();
           $identity2 = get_object_vars($identity);
-          
+
         }
-        
+
         $this->modelUsuario = new Application_Model_Usuario();
         $this->usuario = $this->modelUsuario->selectLogin($identity2['tl_id']);
         $this->view->assign("usuario",$this->usuario);
-        
+
         $this->modelIsca = new Application_Model_Isca();
     }
 
     public function indexAction()
     {
-        
+
         $dadosIsca = $this->modelIsca->select(NULL, 'isc_tipo', NULL);
 
         $this->view->assign("dadosIscas", $dadosIsca);
     }
-    
+
     public function insertAction() {
         if($this->usuario['tp_id']==15 | $this->usuario['tp_id'] ==17 | $this->usuario['tp_id']==21){
             $this->_redirect('index');
@@ -62,6 +62,37 @@ class IscaController extends Zend_Controller_Action
         $this->_redirect('isca/index');
         }
     }
+   	public function relatoriolistaAction() {
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
 
+		$localModelIsca = new Application_Model_Isca();
+
+		$localIsca = $localModelIsca->select(NULL, array('isc_tipo'), NULL);
+
+		require_once "../library/ModeloRelatorio.php";
+		$modeloRelatorio = new ModeloRelatorio();
+		$modeloRelatorio->setTitulo('Relatório de Iscas');
+
+		$modeloRelatorio->setLegenda(30, 'Código', '');
+		$modeloRelatorio->setLegenda(80, 'Isca', '');
+
+		foreach ( $localIsca as $key => $localData ) {
+			$modeloRelatorio->setValueAlinhadoDireita(30, 40, $localData['isc_id']);
+			$modeloRelatorio->setValue(80, $localData['isc_tipo']);
+
+			$modeloRelatorio->setNewLine();
+		}
+
+		$modeloRelatorio->setNewLine();
+		$pdf = $modeloRelatorio->getRelatorio();
+
+// 		header('Content-Disposition: attachment;filename="rel_familia.pdf"');
+// 		header("Content-type: application/x-pdf");
+// 		echo $pdf->render();
+
+		header("Content-Type: application/pdf");
+		echo $pdf->render();
+   }
 }
 
--- application/models/Pescador.php
+++ application/models/Pescador.php
@@ -466,4 +466,14 @@ class Application_Model_Pescador {
         return $db->fetchAll($select)->toArray();
     }
 
+    public function select_Pescador_group_colonia() {
+        $db = new Application_Model_DbTable_VPescador();
+        $select = $db->select()
+                ->from('v_pescador', array('count(*)','tc_id', 'tc_nome'))
+                ->group(array('tc_id', 'tc_nome'))
+                ->order('tc_nome');
+        
+        return $db->fetchAll($select)->toArray();
+    }
+    
 }
