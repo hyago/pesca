6acc100d5b654ac640497cd5264992bebe9d539a
stefano <stefanouesc@gmail.com>
2014-11-17 11:53:33 -0300

    Coleta Manual

--- application/controllers/RelatoriosController.php
+++ application/controllers/RelatoriosController.php
@@ -95,9 +95,9 @@ class RelatoriosController extends Zend_Controller_Action
         $sheet->addChart($chart);
         
         $writer = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-        ob_end_clean();
-        $writer->setIncludeCharts(TRUE);
         
+        $writer->setIncludeCharts(TRUE);
+        ob_end_clean();
         header('Content-Type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename="grafico_phpexcel_chart_class.xls"');
         header('Cache-Control: max-age=0');
